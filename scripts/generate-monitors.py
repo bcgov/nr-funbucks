3c109d99a2a9185ae02d2e9eb4311ee70acda2a1
sanjbabu <sanjay.babu@gov.bc.ca>
2022-06-10 14:01:02 -0700

    tabs vs spaces!

--- scripts/generate-monitors.py
+++ scripts/generate-monitors.py
@@ -4,7 +4,7 @@ ipFileHandler = open("fluentbit_agents.csv", "r")
 numLines = sum(1 for line in ipFileHandler)
 ipFileHandler.seek(0)
 opFileHandler = open("../output/monitors.json", "w")
-opFileHandler.write("[\n\t")
+opFileHandler.write("[\n  ")
 j = 0
 for line in ipFileHandler:
     arr = line.split(",")
@@ -12,16 +12,16 @@ for line in ipFileHandler:
     agentCount = int(arr[1])
     j = j + 1
     for i in range(agentCount): 
-        opFileHandler.write("{\n\t")
+        opFileHandler.write("{\n  ")
         opFileHandler.write('  "name": "nrm_' + server + '_fluent-bit.' + str(i) + '",')
-        opFileHandler.write("\n\t")
+        opFileHandler.write("\n  ")
         opFileHandler.write('  "server": "'+ server + '",')
-        opFileHandler.write("\n\t")
+        opFileHandler.write("\n  ")
         opFileHandler.write('  "agent": ' + '"fluent-bit.' + str(i) + '"')
-        opFileHandler.write("\n\t")
+        opFileHandler.write("\n  ")
         opFileHandler.write("}")
         if(j!=numLines or (j==numLines and i==agentCount)):
-            opFileHandler.write(",\n\t")
+            opFileHandler.write(",\n  ")
 opFileHandler.write("\n]")
 opFileHandler.close()
 ipFileHandler.close()
