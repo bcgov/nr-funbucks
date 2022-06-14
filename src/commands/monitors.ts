53340d457bd4716d3b4f5c26f7ea297e642fd5c8
sanjbabu <sanjay.babu@gov.bc.ca>
2022-06-14 16:18:19 -0700

    string replaced with array

--- src/commands/monitors.ts
+++ src/commands/monitors.ts
@@ -1,50 +1,54 @@
-import {Command, Flags} from '@oclif/core';
-import * as fs from 'fs';
-const path = require('path');
+import { Command, Flags } from "@oclif/core";
+import * as fs from "fs";
+const path = require("path");
 
 export default class Monitors extends Command {
-  static description = 'generate monitor configuration'
+  static description = "generate monitor configuration";
 
-  static examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
+  static examples = ["<%= config.bin %> <%= command.id %>"];
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Monitors);
-    const monitorJson = function(): string {
-      const ipFile = fs.readFileSync(path.join('scripts','fluentbit_agents.csv'), 'utf-8').trim();
-      const serverList = ipFile.split('\n');
-      let op = '[\n  ';
-      for (let i in serverList) {
-        const server = serverList[i].split(',')[0];
-        const agentCount = Number(serverList[i].split(',')[1]);
-        for (let j=0; j<agentCount; j++)
-          { 
-              op += "{\n  ";
-              op += '  "name": "nrm_' + server + '_fluent-bit.' + j + '",'
-              op += "\n  ";
-              op += '  "server": "'+ server + '",'
-              op += "\n  ";
-              op += '  "agent": ' + '"fluent-bit.' + j + '"';
-              op += "\n  ";
-              op += "}";
-              if(!(Number(i)>=serverList.length-1 && j==agentCount-1)) {
-                  op += ",\n  ";
-              }
-          }  
+
+    type Monitor = {
+      name: string;
+      server: string;
+      agent: string;
+    };
+
+    const { args, flags } = await this.parse(Monitors);
+    const monitorJson = function (): string {
+      let monitorsList: Monitor[] = [];
+      const ipFile = fs
+        .readFileSync(path.join("scripts", "fluentbit_agents.csv"), "utf-8")
+        .trim();
+      const serverList = ipFile.split("\n");
+      let op = "";
+      for (let i of serverList) {
+        const serverName = i.split(",")[0];
+        const agentCount = Number(i.split(",")[1]);
+        for (let j = 0; j < agentCount; j++) {
+          const monitor: Monitor = {
+            name: "nrm_" + serverName + "_fluent-bit." + j,
+            server: serverName,
+            agent: "fluent-bit." + j,
+          };
+          monitorsList.push(monitor);
+        }
       }
-      op +=  "\n]";
-      return op;
-  }
-  
-  fs.writeFile('output/monitors.json', monitorJson(),  function(err) {
-      if (err) {
+      return JSON.stringify(monitorsList, null, 2);
+    };
+
+    fs.writeFile(
+      path.join("output", "monitors.json"),
+      monitorJson(),
+      function (err) {
+        if (err) {
           return console.error(err);
+        }
+        console.log("monitors.json created in the output folder");
       }
-      console.log("monitors.json created in the output folder");
-  });
-  
+    );
   }
 }
