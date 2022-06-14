579900b9f89cd6d643801fe0c97764de82784037
sanjbabu <sanjay.babu@gov.bc.ca>
2022-06-14 16:34:12 -0700

    truncated at 20 chars

--- src/commands/monitors.ts
+++ src/commands/monitors.ts
@@ -35,8 +35,8 @@ export default class Monitors extends Command {
             name: "nrm_" + serverName + "_fluent-bit." + j,
             server: serverName,
             agent: "fluent-bit." + j,
-            id1: crypto.createHash('sha256').update(serverName+j+'1').digest('hex'),
-            id2: crypto.createHash('sha256').update(serverName+j+'2').digest('hex'),
+            id1: crypto.createHash('sha256').update(serverName+j+'1').digest('hex').substring(0,20),
+            id2: crypto.createHash('sha256').update(serverName+j+'2').digest('hex').substring(0,20),
           };
           monitorsList.push(monitor);
         }
