fc3ac9c686d5778f7ca4b0cb5a68a52551ee3447
sanjbabu <sanjay.babu@gov.bc.ca>
2022-06-15 13:48:29 -0700

    ci workflow fixes

--- src/commands/monitors.ts
+++ src/commands/monitors.ts
@@ -1,6 +1,6 @@
-import { Command, Flags } from "@oclif/core";
-import * as fs from "fs";
-const path = require("path");
+import {Command} from '@oclif/core';
+import * as fs from 'fs';
+const path = require('path');
 const crypto = require('crypto');
 const QUERY_LEVEL_TRIGGER_ID_1 = 1;
 const TEAMS_CHANNEL_ACTION_ID_2 = 2;
@@ -9,11 +9,11 @@ const JSON_FORMAT_SPACE_COUNT = 2;
 const ID_MAX_LENGTH = 20;
 
 export default class Monitors extends Command {
-  static description = "generate monitor configuration";
+  static description = 'generate monitor configuration';
 
-  static examples = ["<%= config.bin %> <%= command.id %>"];
+  static examples = ['<%= config.bin %> <%= command.id %>'];
 
-  static args = [{ name: "filePath" }];
+  static args = [{name: 'filePath'}];
 
   public async run(): Promise<void> {
 
@@ -26,23 +26,23 @@ export default class Monitors extends Command {
       automation_queue_action_id: number;
     };
 
-    const { args, flags } = await this.parse(Monitors);
+    const {args} = await this.parse(Monitors);
 
-    const filePath = args["filePath"];
+    const filePath = args['filePath'];
     const monitorJson = function (): string {
       let monitorsList: Monitor[] = [];
       const ipFile = fs
-        .readFileSync(filePath, "utf-8")
+        .readFileSync(filePath, 'utf-8')
         .trim();
-      const serverList = ipFile.split("\n");
+      const serverList = ipFile.split('\n');
       for (let i of serverList) {
-        const serverName = i.split(",")[0];
-        const agentCount = Number(i.split(",")[1]);
+        const serverName = i.split(',')[0];
+        const agentCount = Number(i.split(',')[1]);
         for (let j = 0; j < agentCount; j++) {
           const monitor: Monitor = {
-            name: "nrm_" + serverName + "_fluent-bit." + j,
+            name: 'nrm_' + serverName + '_fluent-bit.' + j,
             server: serverName,
-            agent: "fluent-bit." + j,
+            agent: 'fluent-bit.' + j,
             query_level_trigger_id: crypto.createHash('sha256').update(serverName+j+QUERY_LEVEL_TRIGGER_ID_1).digest('hex').substring(0,ID_MAX_LENGTH),
             teams_channel_action_id: crypto.createHash('sha256').update(serverName+j+TEAMS_CHANNEL_ACTION_ID_2).digest('hex').substring(0,ID_MAX_LENGTH),
             automation_queue_action_id: crypto.createHash('sha256').update(serverName+j+AUTOMATION_QUEUE_ACTION_ID_3).digest('hex').substring(0,ID_MAX_LENGTH),
@@ -54,13 +54,13 @@ export default class Monitors extends Command {
     };
 
     fs.writeFile(
-      path.join("output", "monitors.json"),
+      path.join('output', 'monitors.json'),
       monitorJson(),
       function (err) {
         if (err) {
           return console.error(err);
         }
-        console.log("monitors.json created in the output folder");
+        console.log('monitors.json created in the output folder');
       }
     );
   }
