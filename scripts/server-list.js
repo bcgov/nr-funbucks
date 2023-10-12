dcb8a72b2b432b4466b8c8b6a2d1fc36dc768314
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2023-10-12 09:12:49 -0700

    Server list script

--- scripts/server-list.js
+++ scripts/server-list.js
@@ -1,13 +1,16 @@
+// Prints a list of fluent bit servers, formatted for Jenkins fluent bit job parameters
 const fs = require('fs');
 
 const testFolder = __dirname + '/../config/server/';
 
 fs.readdir(testFolder, (err, files) => {
+  let servers = '';
   files
     .filter(file => file !== 'localhost.json')
     .filter(file => file.endsWith('.json'))
     .map(file => file.slice(0, -5))
     .forEach(file => {
-    console.log(file);
-  });
+      servers += `'${file}', `;
+    });
+  console.log(servers.slice(0, -2));
 });
