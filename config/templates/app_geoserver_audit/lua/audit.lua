db88055b97eb03e627760accd90adb6d917eae92
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2024-08-22 12:12:55 -0700

    feat: add geoserver audit

---
+++ config/templates/app_geoserver_audit/lua/audit.lua
@@ -0,0 +1,33 @@
+-- Start audit.lua
+
+function copy_to_new_message(src, dest, record, new_record)
+    if record[src] ~= nil then
+        new_record[dest] = record[src]
+    end
+end
+
+function construct_log_message(tag, timestamp, record)
+    new_record = {}
+    copy_to_new_message("Path", "url.path", record, new_record)
+    copy_to_new_message("QueryString", "url.query", record, new_record)
+    copy_to_new_message("id", "http.request.id", record, new_record)
+    copy_to_new_message("HttpMethod", "http.request.method", record, new_record)
+    copy_to_new_message("ResponseStatus", "http.response.status_code", record, new_record)
+    copy_to_new_message("ResponseLength", "http.response.body.bytes", record, new_record)
+    copy_to_new_message("ResponseContentType", "http.response.mime_type", record, new_record)
+    copy_to_new_message("TotalTime", "event.duration", record, new_record)
+    copy_to_new_message("RemoteAddr", "source.ip", record, new_record)
+    copy_to_new_message("RemoteHost", "source.domain", record, new_record)
+    copy_to_new_message("StartTime", "event.start", record, new_record)
+    copy_to_new_message("EndTime", "event.end", record, new_record)
+    copy_to_new_message("Service", "event.provider", record, new_record)
+
+    if record["Failed"] == "false" then
+        new_record["event.outcome"] = "success"
+    end
+    if record["Failed"] == "true" then
+        new_record["event.outcome"] = "failure"
+    end
+
+    return 2, timestamp, new_record
+end
