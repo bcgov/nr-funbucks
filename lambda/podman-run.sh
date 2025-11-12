36bc853bd4e203496d44f2b44489ba74a0222169
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2025-11-12 10:13:11 -0800

    feat: wildfire log changes (#809)

--- lambda/podman-run.sh
+++ lambda/podman-run.sh
@@ -14,7 +14,7 @@ export HOST_HOSTNAME="$(hostname -s)"
 export HOST_ID="$(hostname -f)"
 export HOST_NAME="${HOST_HOSTNAME}"
 export HOST_DOMAIN="$(echo ${HOST_HOSTNAME#[[:alpha:]]*.})"
-export FLUENT_VERSION="4.0.9"
+export FLUENT_VERSION="4.1.1"
 export FLUENT_CONF_HOME="/config"
 export FUNBUCKS_HOME="${PWD}/.."
 
